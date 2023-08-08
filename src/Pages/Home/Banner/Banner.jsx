fa3ee1d104d5c5c648ed56891f5daff2f1c60566
shukumar-542 <shukumar542@gmail.com>
2023-08-08 21:55:44 +0600

    update some functionality

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper";
 import "swiper/css";
 import './banner.css'
 import slider1 from '../../../assets/rugby.jpg'
@@ -8,12 +7,24 @@ import slider2 from '../../../assets/swim-bg.jpg'
 import slider3 from '../../../assets/cover1.jpg'
 import 'swiper/css/navigation';
 import { Fade } from 'react-awesome-reveal';
+import { Pagination, Navigation, Autoplay } from 'swiper';
+
 
 const Banner = () => {
     return (
         <div className='h-[450px] '>
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-            <SwiperSlide>
+            <Swiper
+                pagination={{
+                    type: 'fraction',
+                }}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation, Autoplay]}
+                className="mySwiper">
+                <SwiperSlide>
                     <img src={slider2} alt="" />
                     <div className='absolute text-white top-32 md:left-[50%] w-1/3'>
 
@@ -22,7 +33,7 @@ const Banner = () => {
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    
+
 
                     <img src={slider3} className='object-contain' alt="" />
                     <div className='absolute text-white top-28 md:left-20 w-1/3'>
@@ -30,9 +41,9 @@ const Banner = () => {
                         <p className='text-black my-5'>Get professional advise from experts in the sport you love!</p>
                     </div>
                 </SwiperSlide>
-               
+
                 <SwiperSlide>
-                <div className=' relative h-full'>
+                    <div className=' relative h-full w-full'>
                         <img src={slider1} className='bg-opacity-30' alt="" />
                         <div className='absolute text-white top-32 left-20 w-1/3'>
                             <h2 className='md:text-6xl font-bold'>We Create History</h2>
