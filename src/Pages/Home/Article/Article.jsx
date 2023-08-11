31f4a394cd5a3eeee0ad1e1e5e051fa7dc3e96ea
shukumar-542 <shukumar542@gmail.com>
2023-08-11 19:46:18 +0600

    update article section

--- src/Pages/Home/Article/Article.jsx
+++ src/Pages/Home/Article/Article.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ar1 from '../../../assets/articel1.jpg'
 import ar2 from '../../../assets/ar2.jpg'
 import ar3 from '../../../assets/atricle3.jpg'
+import 'aos/dist/aos.css'
+import AOS from 'aos';
+AOS.init();
 
 const Article = () => {
     return (
@@ -10,13 +13,13 @@ const Article = () => {
                 <h1 className='text-4xl font-bold '><span className='text-blue-600'>LATEST BLOG</span> & ARTICLES</h1>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-5 my-10'>
-                <div className='row-span-3 flex flex-col justify-end pb-14 opacity-95' style={{ backgroundImage: `url(${ar3})` }}>
+                <div   className='row-span-3 flex flex-col justify-end pb-14 opacity-95' style={{ backgroundImage: `url(${ar3})` }}>
 
                     <h1 className='text-4xl text-white font-bold px-10'>REASONS WHY YOU FACE OBSTACLES IN LEARNING BADMINTON</h1>
                     <p className='text-white px-10'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean...</p>
 
                 </div>
-                <div className='flex flex-col justify-between  gap-10 h-full'>
+                <div  className='flex flex-col justify-between  gap-10 h-full'>
                     <div className='grid grid-cols-2 gap-5 justify-between items-center'>
                         <img src={ar1} alt="" />
                         <div className='space-y-5'>
--- src/components/PopularInstructor/SingleInstructor.jsx
+++ src/components/PopularInstructor/SingleInstructor.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
-const SingleInstructor = ({instructor}) => {
+const SingleInstructor = ({ instructor }) => {
     console.log(instructor);
 
     return (
-        <div className=" bg-white shadow-xl p-4">
-            <figure><img src={instructor.image} alt="Shoes" className='w-full h-52' /></figure>
+        <div className=" bg-white shadow-xl group ">
+            <div className='h-52 overflow-hidden'>
+                <figure><img src={instructor.image} alt="Shoes" className='w-full h-52 group-hover:scale-110 transition duration-500' /></figure>
+            </div>
             <div className="card-body">
                 <h2 className="card-title text-blue-500">Name : {instructor.name}</h2>
                 <p>Email : {instructor.email}</p>
-                
-                
+
+
             </div>
         </div>
     );
