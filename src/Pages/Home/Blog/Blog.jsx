4c0bdc1ccc9435de434d888cf48bd2505de8f0ca
shukumar-542 <shukumar542@gmail.com>
2023-08-16 18:12:27 +0600

    update our class section

--- src/Pages/Home/Blog/Blog.jsx
+++ src/Pages/Home/Blog/Blog.jsx
@@ -4,9 +4,9 @@ import img from '../../../assets/bad.jpg'
 
 const Blog = () => {
     return (
-        <div className='my-container'>
+        <div className='my-10'>
             <Parallax bgImage={img} strength={200} >
-                <div className=" min-h-screen flex items-center ps-20" >
+                <div className="py-36 flex items-center ps-20" >
                     <div className='w-1/2 space-y-6 '>
                         <h1 className='text-white text-4xl font-bold'> <span className='text-blue-600'>JOIN US NOW </span> & GET FREE TRAINING!</h1>
                         <p className='text-white'>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
--- src/components/PopularClass/SingleClass.jsx
+++ src/components/PopularClass/SingleClass.jsx
@@ -1,20 +1,48 @@
 import React from 'react';
+import './style.css'
 
 const SingleClass = ({ item }) => {
+    console.log(item);
     return (
-        <div className="shadow-md group rounded-lg overflow-hidden ">
-            <div className='overflow-hidden'>
-                <figure><img src={item.image} alt="" className='w-full  h-52 group-hover:scale-110 transition  duration-700 ' /></figure>
-            </div>
-            <div className="card-body">
-                <h2 className="text-2xl font-semibold uppercase">{item.instructor}</h2>
-                <div className='flex'>
-                    <p className='uppercase font-semibold'>price : ${item.price} </p>
-                    <p className='uppercase font-semibold'>available Seat : {item.seat}</p>
-                </div>
+        // <div className="shadow-md group rounded-lg overflow-hidden ">
+        //     <div className='overflow-hidden'>
+        //         <figure><img src={item.image} alt="" className='w-full  h-52 group-hover:scale-110 transition  duration-700 ' /></figure>
+        //     </div>
+        //     <div className="card-body">
+        //         <h2 className="text-2xl font-semibold uppercase">{item.instructor}</h2>
+        //         <div className='flex'>
+        //             <p className='uppercase font-semibold'>price : ${item.price} </p>
+        //             <p className='uppercase font-semibold'>available Seat : {item.seat}</p>
+        //         </div>
 
+        //     </div>
+        // </div>
+
+        <div class="card-container mx-auto">
+            <span class="pro">PRO</span>
+            <div className='flex items-center justify-center'>
+                <img class="round w-32 h-32" src={item.image} alt="user" />
             </div>
+            <h3 className='uppercase font-semibold text-2xl'>{item.instructor}</h3>
+            <h6>New York</h6>
+            <div className='flex items-center justify-center gap-2'>
+                <p className='uppercase font-semibold'>price : ${item.price} </p>
+                <p className='uppercase font-semibold'>available Seat : {item.seat}</p>
+            </div>
+            <div class="buttons space-x-5 py-10">
+                <button class="primary">
+                    Message
+                </button>
+                <button class="primary ghost">
+                    Following
+                </button>
+            </div>
+
         </div>
+
+
+
+
 
     );
 };
